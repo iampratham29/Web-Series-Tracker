a0f659d4bcdd14f842fe09c4c32d1818eddf499b
Vedangi Deepak Deshpande <58439293+Vedangi05@users.noreply.github.com>
2020-05-22 13:54:24 +0530

    Update UI_start.py

--- UI_start.py
+++ UI_start.py
@@ -33,7 +33,7 @@ def insert():
 
         MessageBox.showinfo("Insert Status" ,"Inserted successfully");
 
-        con.close();
+        mydb.close();
 
 def delete():
     if(e_series_name.get() ==""):
@@ -60,7 +60,7 @@ def delete():
 
         MessageBox.showinfo("Delete Status" ,"Deleted successfully");
 
-        con.close();
+        mydb.close();
 
 def update():
     series_name=e_series_name.get();
@@ -81,7 +81,7 @@ def update():
         )
 
         mycursor = mydb.cursor()
-        mycursor.execute("update store set category='"+ category +"',star='" ,star +"' where series_name='"+ series_name +"'")
+        mycursor.execute("update store set category='"+ category +"',star='" +star +"' where series_name='"+ series_name +"'")
         mycursor.execute("commit");
 
         e_series_name.delete(0,'end')
@@ -92,20 +92,57 @@ def update():
 
         MessageBox.showinfo("Update Status" ,"Updated successfully");
 
-        con.close();
+        mydb.close();
 
+def get():
+    if(e_series_name.get() == ""):
+        MessageBox.showinfo("Fetch status", "ID is compulsory for delete")
+    else:
+        mydb = mysql.connect(
+        host="localhost",
+        
+        user="root",
+        passwd="admin123",
+        database="dance",
+        auth_plugin='mysql_native_password'
+        )
 
+        mycursor = mydb.cursor()
+        mycursor.execute("select * from store where series_name='" + e_series_name.get() +"'")
+        rows = mycursor.fetchall()
 
+        for row in rows:
+            e_category.insert(0, row[1])
+            e_star.insert(0,row[2])
+        mydb.close();
 
-    
+        
+ 
+def show():
+        mydb = mysql.connect(
+        host="localhost",
+        
+        user="root",
+        passwd="admin123",
+        database="dance",
+        auth_plugin='mysql_native_password'
+        )
 
+        mycursor = mydb.cursor()
+        mycursor.execute("select * from store")
+        mycursor.fetchall()
+
+        for row in rows:
+            insertData = str(row[0])+ '     '+ row[1]
+            list.insert(list.size()+1, insertData)
+        mydb.close()
 
 
 root=Tk()
 root.geometry("600x300")
 root.title("Python+Tkinter+Mysql")
 
-series_name=Label(root, text='Enter Id',font=('bold',10))
+series_name=Label(root, text='Enter series name',font=('bold',10))
 series_name.place(x=20,y=30)
 
 category=Label(root, text='Enter Category',font=('bold',10))
@@ -118,7 +155,7 @@ e_series_name = Entry()
 e_series_name.place(x=150, y=30)
 
 e_category = Entry()
-e_category.place(x=150, y=60)
+e_category.place(x=150,y=60)
 
 e_star= Entry()
 e_star.place(x=150, y=90)
@@ -134,7 +171,10 @@ delete.place(x=70, y=140)
 update =Button(root, text="Update" ,font=("italic" ,10) ,bg="white", command=update)
 update.place(x=130, y=140)
 
-get =Button(root, text="Get" ,font=("italic" ,10) ,bg="white", command=get)
-insert.get(x=190, y=140)
+get =Button(root, text="get" ,font=("italic" ,10) ,bg="white", command=get)
+get.place(x=190, y=140)
+
+list = Listbox(root)
+list.place(x=290,y=30)
 
 root.mainloop()
